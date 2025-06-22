0f47a03344d494636cb8ffd402d911b38e9d2c85
Sumarmahadev <sumarchimacode@gmail.com>
2025-06-22 22:36:31 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
   
-    allow_origins=["https://langtalk.netlify.app"]
-
+    allow_origins=["https://langtalk.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
