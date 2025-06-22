9db55c7995623ee796fc88af1300ff609ae9e230
Sumarmahadev <sumarchimacode@gmail.com>
2025-06-22 15:42:28 +0530

    Update Register.jsx

--- lang/src/components/Register.jsx
+++ lang/src/components/Register.jsx
@@ -13,7 +13,7 @@ function Register({ onRegister }) {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:8000/register", {
+      const res = await fetch("https://langtalk.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
